d5b17566e2b072f7a5a72145b176325d65d04964
Julien Blasco <jvdblasco@gmail.com>
2015-12-31 01:34:40 -0500

    Rajout import SQLContext et ajout de prints

--- script1.py
+++ script1.py
@@ -15,6 +15,8 @@ from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.tuning import ParamGridBuilder
 from pyspark.ml.tuning import CrossValidator
+from pyspark.sql import SQLContext 
+
 
 #create Sparkcontext
 sc = SparkContext(appName="Simple App")
@@ -27,9 +29,13 @@ def cleanLower(doc):
     return doc.replace("<br /><br />"," ").lower()
 rdd = labeledRdd.map(lambda doc : (cleanLower(doc[0]),doc[1]))
 
+print "Text is cleaned"
+
 sqlContext = SQLContext(sc)
 df = sqlContext.createDataFrame(rdd, ['review', 'label'])
 dfTrain, dfTest = df.randomSplit([0.8,0.2])
+
+print "Random split is done"
 
 tokenizerNoSw = tr.NLTKWordPunctTokenizer(
     inputCol="review", outputCol="wordsNoSw",  
@@ -51,20 +57,19 @@ grid=(ParamGridBuilder()
      .baseOn([evaluator.metricName,'precision'])
      .addGrid(dt.maxDepth, [10,20])
      .build())
+
+print "Grid is build"
+
 cv = CrossValidator(estimator=pipeline, estimatorParamMaps=grid,evaluator=evaluator)
 
+print "CV Estimator is defined"
+
 cv_model = cv.fit(dfTrain)
+
+print "Model is fitted"
+
 df_test_pred = cv_model.transform(dfTest)
+
+print "Labels are predicted"
+
 evaluator.evaluate(df_test_pred)
-
-
-
-
-
-
-
-
-
-
-
-
