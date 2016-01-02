bc6bc67902ba5622b9d12a963e5b514cfa320d9a
Julien Blasco <jvdblasco@gmail.com>
2016-01-01 20:12:17 -0500

    ajout fonction de sauvegarde du modele

--- main_reglogit.py
+++ main_reglogit.py
@@ -108,3 +108,15 @@ print "Pourcentage de bonne classification(0-1): ",resultat
 #	output.write('%s\t%d\n'%x)
 #output.close()
 
+# Ceci est un bout de code qu'il faut coller à la fin du script
+# qui entraine le modele.
+# Cela va creer un fichier texte 'sauvegarde_MYMODEL' (a renommer en fonction du modele)
+# qui contiendra le modele deja entraine
+
+# remplacer MYMODELINSTANCE par le modele fitte sur les donnees
+
+import pickle
+
+output=file('./sauvegarde_MYMODEL','w')
+pickle.dump(MYMODELINSTANCE,output)
+output.close()
