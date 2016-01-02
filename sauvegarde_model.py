4ebb13039a0bc87ac3c6374d559fdf639bd9f65e
Julien Blasco <jvdblasco@gmail.com>
2016-01-01 20:09:56 -0500

    suppression accent

--- sauvegarde_model.py
+++ sauvegarde_model.py
@@ -3,7 +3,7 @@
 # Cela va creer un fichier texte 'sauvegarde_MYMODEL' (a renommer en fonction du modele)
 # qui contiendra le modele deja entraine
 
-# remplacer MYMODELINSTANCE par le modele fitte sur les données
+# remplacer MYMODELINSTANCE par le modele fitte sur les donnees
 
 import pickle
 
